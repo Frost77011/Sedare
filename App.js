0c4d1332ad459b0e46634635d33602790296070d
AmmonWarnick <warnick.ammon@gmail.com>
2022-03-25 18:57:24 -0600

    add bottom nav

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 
 import HomeScreen from './Screens/HomeScreen';
@@ -14,7 +15,7 @@ export default class App extends React.Component {
   }
 }
 
-const AppNavigator = createStackNavigator({
+const AppNavigator = createBottomTabNavigator({
   Home: {
     screen: HomeScreen
   },
